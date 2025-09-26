f6d48edd1bb469e5ea200ca0f6f25049c58d8fea
10.168.3.171\bill.w.chen <Fubon68023>
2025-09-26 14:50:59 +0800

    增加remark 解說文字

--- src/App2.vue
+++ src/App2.vue
@@ -1,7 +1,9 @@
 <template>
   {{ message }}
+  <!-- v-once 只渲染一次不再更動 -->
   <p v-once>{{ message }}</p>
   <p v-pre>{{ message }}</p>
+  <!-- if else 使用 -->
   <p v-if="isShow">Hello World</p>
   <p v-else>Hello Taiwan</p>
   <p v-if="x === 0">x = 0</p>
@@ -9,32 +11,37 @@
   <p v-else-if="x === 2">x = 2</p>
   <p v-else>x != 0</p>
   <p v-show="isShow">Hello Taipei</p>
+  <!-- for 迴圈使用 一定要設定key -->
   <p v-for="user in users1" :key="user">Welcome {{ user }}</p>
   <p v-for="(user, index) in users2" :key="user.email">
     {{ index }} {{ user.name }} {{ user.email }}
   </p>
-
+<!-- for 迴圈 + if 使用 -->
   <div v-for="(user, index) in users1" :key="user">
     <p v-if="user === 'Jake'">Welcome {{ index }} {{ user }}</p>
   </div>
 
   {{ x * y }}
   <br>
+  <!-- function 使用 -->
   {{ myFunction(10) }} 
   <br>
+  <!-- 按鈕使用更改畫面 -->
   {{ name }}
   {{ count }} <br>
   <button @click="countFunction(2), changeName()">記數和改名</button>
   <br />
+  <!-- v-model.lazy 使用，輸入完畢才更改 --> 
   <input type="text" v-model.lazy="name" /><br>
   <button @click="addUser">新增使用者</button>
   <br />
   <input type="text" v-model="message" />
   <br />
+  <!-- for 搭配輸入欄 -->
   <p v-for="text in text1" :key="text">{{ text }}</p><br />
   <textarea v-model.lazy="text" /><br />
   <button @click="addMessage">新增文字</button>
-
+  <!-- 下拉式選單 -->
   <br>{{ selectValue }} <br />
   <select v-model="selectValue">
     <option value="">select</option>
@@ -42,30 +49,26 @@
     <option value="2">2</option>
     <option value="3">3</option>
   </select>
-
+  <!-- checkbox -->
   <br>{{ isCheck }} <br />
   <input type="checkbox" v-model="isCheck" /> is check
-
+  <!-- 多選checkbox -->
   <br>{{ checkbox }} <br />
   <input type="checkbox" value="0" v-model="checkbox" /> 0
   <input type="checkbox" value="1" v-model="checkbox" /> 1
   <input type="checkbox" value="2" v-model="checkbox" /> 2
   <input type="checkbox" value="3" v-model="checkbox" /> 3
-
+  <!-- radio 單選-->
   <br>{{ radio }} <br />
   <input type="radio" value="0" v-model="radio" /> 0
   <input type="radio" value="1" v-model="radio" /> 1
   <input type="radio" value="2" v-model="radio" /> 2
   <input type="radio" value="3" v-model="radio" /> 3
 
-  <br>{{ formData.name }}
-
-  <br>
-    <input type="text" v-model="formData.name" />
-  <br />
-
-  <textarea v-model="formData.text" />
-  <br />
+  <!-- 表格 -->
+  <br>{{ formData.name }} <br />表格名稱
+  <br><input type="text" v-model="formData.name" placeholder="請輸入表格名稱" /><br />
+  <textarea v-model="formData.text" /><br />
 
   <select v-model="formData.selectValue">
     <option value="">select</option>
@@ -75,13 +78,12 @@
   </select>
   <br />
 
-  <input type="checkbox" v-model="formData.isCheck" /> is check
-  <br />
-
-  <input type="checkbox" value="0" v-model="formData.checkbox" /> 0
-  <input type="checkbox" value="1" v-model="formData.checkbox" /> 1
-  <input type="checkbox" value="2" v-model="formData.checkbox" /> 2
-  <input type="checkbox" value="3" v-model="formData.checkbox" /> 3
+  <input type="checkbox" v-model="formData.isCheck2" /> is check
+  <br />{{ formData.checkbox2 }} <br />
+  <input type="checkbox" value="0" v-model="formData.checkbox2" /> 0
+  <input type="checkbox" value="1" v-model="formData.checkbox2" /> 1
+  <input type="checkbox" value="2" v-model="formData.checkbox2" /> 2
+  <input type="checkbox" value="3" v-model="formData.checkbox2" /> 3
   <br />
 
   <input type="radio" value="0" v-model="formData.radio" /> 0
@@ -90,6 +92,7 @@
   <input type="radio" value="3" v-model="formData.radio" /> 3
   <br />
   <button @click="submit">送出</button>
+  <!-- computed 範例 -->
   <br /><div v-for="data in checkDatas" :key="data.id">
     {{ data.id }} {{ data.name }}<br />
   </div>
@@ -115,9 +118,11 @@ export default {
   data() {
     return {
       message: 'Hello World',
+      // 確定上方顯示 Hello World or Hello Taiwan
       isShow: true,
-      x: 10,
+      x: 1,
       y: 3,
+      // 搭配上方for迴圈使用
       users1: ['Jake', 'Allan', 'Eason'],
       users2: [
         { name: 'Jake', email: 'jake@gmail.com' },
@@ -129,16 +134,20 @@ export default {
       text1: [],
       text: '',
       text2: '',
+      //搭配下拉式選單使用
       selectValue: '',
+      //搭配checkbox使用
       isCheck: false,
+      //紀錄check item
       checkbox: [],
+      //搭配radio使用
       radio: '',
       formData: {
         name: '',
         text: '',
         selectValue: '',
-        isCheck: false,
-        checkbox: [],
+        isCheck2: false,
+        checkbox2: [],
         radio: '',
       },
       datas: [
@@ -151,6 +160,7 @@ export default {
       },
       };
     },
+    // function 定義在 methods 內
     methods: {
       myFunction(value) {
         return this.x * this.y + value;
@@ -168,7 +178,7 @@ export default {
         }
       },
       addMessage() {
-      if (this.text && !this.text1.includes(this.text)) {
+      if (!this.text1.includes(this.text)) {
         this.text1.push(this.text);
         }
       },
@@ -179,12 +189,14 @@ export default {
       this.text2 = text;
       },
     },
+    // computed 會有快取功能，當依賴的資料沒有改變時，不會重新計算
     computed: {
     checkDatas() {
         console.log("只執行一次且vue3會自動管理不需要額外賦值");
       return this.datas.filter((data) => data.id === 1);
     },
   },
+  // watch 會在資料改變時執行對應的函式
   watch: {
   count(newValue, oldValue) {
     console.log(newValue, oldValue);
@@ -202,6 +214,7 @@ export default {
       deep: true, //要監聽陣列時，要設定才會起作用
     },
   },
+  // 要跨檔案要在這邊設定
   components: {
     HelloWorld,
     AppHeader,
